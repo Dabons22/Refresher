89acf7fc20605795da948488ca73fbccfa4be949
daniel <dabons22@gmail.com>
2024-11-03 13:42:45 +0800

    Revert "Initial commit adding new project with basic HTTP Method"
    This reverts commit 0457261d8a04d2f967bda85cb55d17c90aaaea4a.

--- src/main/java/com/refresher/refresher/repository/PlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.refresher.refresher.repository;
-
-import com.refresher.refresher.entities.Places;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PlaceRepository  extends JpaRepository<Places,Long> {
-
-}
